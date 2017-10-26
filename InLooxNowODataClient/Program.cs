ce17d9ef4de3d2b8a8622b5c2e46ea765f291d9b
Marinus Pfund <pfund@inloox.net>
2017-10-26 09:36:26 +0200

    initial checkin

---
+++ InLooxNowODataClient/Program.cs
@@ -0,0 +1,79 @@
+﻿using InLooxODataV9;
+using IQmedialab.InLoox.Data.BusinessObjects;
+using Microsoft.OData.Client;
+using System;
+using System.Linq;
+
+namespace InLooxnowClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var endPoint = new Uri("https://app.inlooxnow.com/");
+            var endPointOdata = new Uri(endPoint, "odata/");
+
+            var username = "user@inloox.com";
+            var password = "";
+
+            var tokenResponse = ODataBasics.GetToken(endPoint, username, password).Result;
+
+            // in case multiple accounts exists
+            if (tokenResponse.Error != null)
+            {
+                var accounts = tokenResponse.GetAccounts();
+
+                // filter correct account by name
+                var myAccount = accounts.FirstOrDefault(k => k.Name.StartsWith("000000"));
+                tokenResponse = ODataBasics.GetToken(endPoint, username, password, myAccount.Id).Result;
+            }
+
+            if (tokenResponse == null)
+            {
+                Console.WriteLine("Login invalid");
+                return;
+            }
+
+            var context = ODataBasics.GetInLooxContext(endPointOdata, tokenResponse.AccessToken);
+            // lookup custom field "DocTest" id
+            var ceDefaults = context.customexpanddefaultextend.ToList();
+            var cedDocument = ceDefaults.FirstOrDefault(k => k.DisplayName == "DocTest");
+
+            if (cedDocument == null)
+            {
+                Console.WriteLine("Custom field 'DocTest' not found");
+                return;
+            }
+
+            // build query for all documents with custom field 'DocTest' set to true.
+            var query = context.documentview
+                .Where(k => k.CustomExpand.Any(ce => ce.CustomExpandDefaultId == cedDocument.CustomExpandDefaultId && ce.BoolValue == true))
+                .OrderBy(k => k.FileName)
+                .Skip(0)
+                .Take(10);
+
+            // need to use DataServiceCollection to use the PostOnlySetProperties feature
+            // if you only need to read from the query a ToList() is ok.
+            var docs = new DataServiceCollection<DocumentView>(query);
+
+            Console.WriteLine(docs.Count());
+
+            foreach (var d in docs)
+                Console.WriteLine(d.FileName);
+
+            // change document state
+            if (docs.Count > 0)
+            {
+                var doc = docs.FirstOrDefault();
+                doc.State = "Updated " + DateTime.Now;
+
+                // only update the modfied properties
+                context.SaveChangesDefaultOptions = SaveChangesOptions.PostOnlySetProperties;
+
+                context.UpdateObject(doc);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ InLooxODataV9/ODataBasics.cs
@@ -0,0 +1,90 @@
+﻿using Default;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InLooxODataV9
+{
+    public static class ODataBasics
+    {
+        public class TokenResponse
+        {
+            [JsonProperty(PropertyName = "error")]
+            public string Error { get; private set; }
+
+            [JsonProperty(PropertyName = "error_description")]
+            public string ErrorDescription { get; private set; }
+
+            [JsonProperty(PropertyName = "access_token")]
+            public string AccessToken { get; private set; }
+
+            [JsonProperty(PropertyName = "token_type")]
+            public string TokenType { get; private set; }
+
+            [JsonProperty(PropertyName = "expires_in")]
+            public int ExpiresIn { get; private set; }
+
+            public TokenAccount[] GetAccounts()
+            {
+                var accountsText = ErrorDescription;
+                var entries = accountsText.Split('#');
+                var accounts = entries.Select(k =>
+                {
+                    var splitted = k.Split('|');
+                    return new TokenAccount(splitted[1], Guid.Parse(splitted[0]));
+                }).ToArray();
+
+                return accounts;
+            }
+
+            public class TokenAccount
+            {
+                public TokenAccount(string name, Guid id)
+                {
+                    this.Name = name;
+                    this.Id = id;
+                }
+
+                public string Name { get; private set; }
+                public Guid Id { get; private set; }
+            }
+        }
+
+        public static Container GetInLooxContext(Uri odataEndPoint, string bearerToken)
+        {
+            var context = new Container(odataEndPoint);
+            context.SendingRequest2 += (sender, eventArgs) =>
+            {
+                eventArgs.RequestMessage.SetHeader("Authorization", "bearer " + bearerToken);
+            };
+
+            return context;
+        }
+
+        public static async Task<TokenResponse> GetToken(Uri endPoint, string username, string password, Guid? clientId = null)
+        {
+            var tokenUrl = new Uri(endPoint, "api/0/token");
+
+            var values = new Dictionary<string, string>{
+                { "username", username },
+                { "password", password },
+                {"grant_type","password" },
+            };
+
+            if (clientId != null)
+                values.Add("client_id", clientId.ToString());
+
+            var content = new FormUrlEncodedContent(values);
+
+            var client = new HttpClient();
+            var response = await client.PostAsync(tokenUrl, content);
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            return JsonConvert.DeserializeObject<TokenResponse>(responseString);
+        }
+    }
+}
